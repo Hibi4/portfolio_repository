60f2a5580769e0a9dc7a4891378cb915065e6baf
Hibi4 <49773@etu.he2b.be>
2024-08-10 18:57:23 +0200

    Start working on Internalization

---
+++ portfolio/src/pages/i18n/ui.ts
@@ -0,0 +1,36 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        
+        'comment': 'Let a comment',
+    },
+    fr: {
+        'comment': 'Laisser un commentaire',
+        /* 'nav.home': 'Accueil',
+        'nav.about': 'À propos', */
+    },
+} as const;
+/* export const languages = {
+    en: 'English',
+    fr: 'Français',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.twitter': 'Twitter',
+    },
+    fr: {
+      'nav.home': 'Accueil',
+      'nav.about': 'À propos',
+    },
+  } as const; */ 
