c53324e53a04ea419caef37cde9750a792dcc835
Ousmane Diallo <49773@etu.he2b.be>
2024-10-21 18:29:19 +0200

    Work on project - Update code, remove comments

--- portfolio/src/pages/i18n/ui.ts
+++ portfolio/src/pages/i18n/ui.ts
@@ -25,21 +25,3 @@ export const ui = {
       'french': 'Français',
     },
 } as const;
-/* export const languages = {
-    en: 'English',
-    fr: 'Français',
-  };
-  
-  export const defaultLang = 'en';
-  
-  export const ui = {
-    en: {
-      'nav.home': 'Home',
-      'nav.about': 'About',
-      'nav.twitter': 'Twitter',
-    },
-    fr: {
-      'nav.home': 'Accueil',
-      'nav.about': 'À propos',
-    },
-  } as const; */ 
